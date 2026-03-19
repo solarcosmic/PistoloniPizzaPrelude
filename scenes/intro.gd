cf167f2e6577da97b44afede733d0acd98014088
Solar <abe@solarcosmic.net>
2026-03-20 10:38:38 +1300

    Add Blocking (v1.0.0)

---
+++ scenes/intro.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@onready var mainui = $MainUI
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MainUI/Label.modulate.a = 0
+	await get_tree().create_timer(1.0).timeout
+	$Camera3D.make_current()
+	$Sounds/ExtremeThunderWithRainKlickaud.play()
+	var tween = create_tween()
+	tween.tween_property($MainUI/Label, "modulate:a", 1.0, 2.0)
+	tween.tween_property($MainUI/Label, "modulate:a", 0.0, 2.0).set_delay(1.5)
+	tween.tween_callback(func(): $MainUI/Label.text = "In association with Quantum Corp")
+	tween.tween_property($MainUI/Label, "modulate:a", 1.0, 2.0)
+	tween.tween_property($MainUI/Label, "modulate:a", 0.0, 2.0).set_delay(1.0)
+	tween.tween_property($MainUI/ColorRect, "modulate:a", 0.0, 2.0)
+	tween.tween_callback(func(): $MainUI/Label.text = "Pistoloni Pizza"; $AnimationPlayer.play("Cutscene1"))
+	tween.tween_property($MainUI/Label, "modulate:a", 1.0, 2.0)
+	tween.tween_property($MainUI/Label, "modulate:a", 0.0, 2.0).set_delay(1.5)
+	tween.tween_property($MainUI/ColorRect, "modulate:a", 1.0, 2.0).set_delay(4.0)
+	tween.tween_callback(func(): get_tree().change_scene_to_file("res://scenes/world.tscn"))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_extreme_thunder_with_rain_klickaud_finished() -> void:
+	print("Restarting rain and thunder sound")
+	$Sounds/ExtremeThunderWithRainKlickaud.play()
--- scenes/world.gd
+++ scenes/world.gd
@@ -4,20 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$MainUI/Label.modulate.a = 0
-	await get_tree().create_timer(1.0).timeout
-	$Camera3D.make_current()
-	$Sounds/ExtremeThunderWithRainKlickaud.play()
-	var tween = create_tween()
-	tween.tween_property($MainUI/Label, "modulate:a", 1.0, 2.0)
-	tween.tween_property($MainUI/Label, "modulate:a", 0.0, 2.0).set_delay(1.5)
-	tween.tween_callback(func(): $MainUI/Label.text = "In association with Quantum Corp")
-	tween.tween_property($MainUI/Label, "modulate:a", 1.0, 2.0)
-	tween.tween_property($MainUI/Label, "modulate:a", 0.0, 2.0).set_delay(1.0)
-	tween.tween_property($MainUI/ColorRect, "modulate:a", 0.0, 2.0)
-	tween.tween_callback(func(): $MainUI/Label.text = "Pistoloni Pizza"; $AnimationPlayer.play("Cutscene1"))
-	tween.tween_property($MainUI/Label, "modulate:a", 1.0, 2.0)
-	tween.tween_property($MainUI/Label, "modulate:a", 0.0, 2.0).set_delay(1.0)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
