6ccbfe6b89d7b45a5601d25acb5c9f9ecf747cb3
Solar <abe@solarcosmic.net>
2026-03-28 16:01:22 +1300

    Add Crosshairs & Bullet Fix (v1.0.0)

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,7 +1,7 @@
 extends Area3D
 
 @export var direction: Vector3
-var speed = 10
+var speed = 30
 
 func _ready() -> void:
 	set_as_top_level(true)
@@ -17,10 +17,13 @@ func _self_destruction():
 	get_tree().create_timer(2).timeout.connect(queue_free)
 
 func _on_area_entered(area: Area3D) -> void:
-	if area.has_method("take_damage"):
-		area.take_damage(1)
-		queue_free()
+	queue_free()
+	#if area.has_method("take_damage"):
+	#	area.take_damage(1)
+	#	queue_free()
 
 
 func _on_body_entered(body: Node3D) -> void:
+	var hit = global_position
+	
 	queue_free()
