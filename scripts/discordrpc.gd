42edbb1bfd14e9288dae2d48bbafa1c037527ecc
Solar <abe@solarcosmic.net>
2026-03-30 19:30:22 +1300

    Add Discord RPC (v1.0.0)

---
+++ scripts/discordrpc.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Discord RPC init")
+	DiscordRPC.app_id = 1276819556232593408
+	DiscordRPC.details = "In Main Menu"
+	DiscordRPC.large_image = "upscaled_pistoloni_pizza_logo_v1"
+	DiscordRPC.large_image_text = "Pistoloni Pizza"
+	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system())
+	DiscordRPC.refresh()
